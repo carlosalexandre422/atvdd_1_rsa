8d3cb6e3174179053fff3a87178c61c7d9f93136
Carlos Alexandre <106106048+carlosalexandre422@users.noreply.github.com>
2024-03-12 18:41:15 -0300

    Update m2.py

--- m2.py
+++ m2.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 
 def main():
-    # Configurar o socket do servidor
+    # Configura o socket do servidor
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('localhost', 12345)  # Altere conforme necessário
     server_socket.bind(server_address)
@@ -15,30 +15,30 @@ def main():
     client_socket, client_address = server_socket.accept()
     print(f"Conexão estabelecida com {client_address}")
 
-    # Receber mensagem da Máquina Virtual 1
+    # Recebe mensagem da Máquina Virtual 1
     received_message = client_socket.recv(4096).decode()
     print("Mensagem recebida da Máquina Virtual 1:", received_message)
 
-    # Encriptar mensagem usando RSA
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
+    # Encripta mensagem usando RSA
+    private_key = rsa.generate_private_key( # gerar uma chave privada RSA
+        public_exponent=65537,              # com um expoente público de 65537
+        key_size=2048,                      # e um tamanho de chave de 2048 bits,
+        backend=default_backend()           # utilizando o backend padrão
     )
 
     public_key = private_key.public_key()
 
-    encrypted_message = public_key.encrypt(
-        received_message.encode(),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
+    encrypted_message = public_key.encrypt( #criptografa usando chave publica
+        received_message.encode(),          #codifica para bytes
+        padding.OAEP(                       #OEAP -> esquema de preenchimento p o rsa assimétrico
+            mgf=padding.MGF1(algorithm=hashes.SHA256()), # método de geração de mascara é o MGF1
+            algorithm=hashes.SHA256(),                   # c o algoritmo de hash SHA256
             label=None
         )
     )
 
-    print(f'Mensagem encriptada {encrypted_message}\n\n')
-    # Serializar a mensagem encriptada e a chave privada antes de enviá-las
+    print(f'Bytes da Mensagem encriptada:\n {encrypted_message}\n\n')
+    # Serializa a mensagem encriptada e a chave privada antes de enviá-las
     serialized_data = pickle.dumps((encrypted_message, private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
@@ -46,8 +46,8 @@ def main():
     )))
 
     client_socket.sendall(serialized_data)
-    print(f'Mensagem serializada: {serialized_data}\n\n')
-    # Fechar o socket do servidor
+    print(f'Bytes da Mensagem encriptada serializada:\n {serialized_data}\n\n')
+    # Fecha o socket do servidor
     server_socket.close()
 
 if __name__ == "__main__":
