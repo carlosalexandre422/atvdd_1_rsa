8565ed0fb34b4fcaa44afeddf4a467757bc77450
Carlos Alexandre <106106048+carlosalexandre422@users.noreply.github.com>
2024-03-12 18:42:12 -0300

    Update m1.py

--- m1.py
+++ m1.py
@@ -1,3 +1,4 @@
+import time
 import socket
 import pickle
 from cryptography.hazmat.backends import default_backend
@@ -5,35 +6,48 @@ from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 
 def main():
-    # Configurar o socket do cliente
+    # Configura o socket do cliente
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = ('localhost', 12345)  # Altere conforme necessário
 
     try:
-        # Tentar conectar ao servidor
+        # Tenta conectar ao servidor
         client_socket.connect(server_address)
 
-        # Enviar mensagem para a Máquina Virtual 2
+        # Envia mensagem para a Máquina Virtual 2
         message_to_send = input("Insira a menssagem a ser encriptada: ")
         client_socket.sendall(message_to_send.encode())
+
+        print('Enviando mensagem...\n')
+
+        for i in range(3):
+            print(i)
+            time.sleep(1)
         print('Mensagem enviada.\n')
 
-        # Receber mensagem encriptada da Máquina Virtual 2
+        # Recebe mensagem encriptada da Máquina Virtual 2
         encrypted_data = client_socket.recv(4096)
-        print(f'Mensagem encriptada recebida: {encrypted_data}\n')
+        print(f'Bytes da Mensagem encriptada recebida: \n{encrypted_data}\n')
 
-        # Desserializar a mensagem encriptada e a chave privada
+        print('Disserializando mensagem...\n')
+        for i in range(3):
+            print(i)
+            time.sleep(1)
+        # Desserializa a mensagem encriptada e a chave privada
         encrypted_message, private_key_bytes = pickle.loads(encrypted_data)
-        print(f'Mensagem disserializada:\nmensagem encriptada disserializada recebida: {encrypted_message}\n\nbytes da chave privada recebida: {private_key_bytes}\n')
-        # Recuperar a chave privada do bytes
+        print(f'Bytes da Mensagem disserializada:\n1. mensagem encriptada disserializada recebida: {encrypted_message}\n\n2. Chave privada recebida: {private_key_bytes}\n')
+        # Recupera a chave privada do bytes
         private_key = serialization.load_pem_private_key(
             private_key_bytes,
-            password=None,  # Se tiver senha, forneça-a aqui
+            password=None, # se tiver senha
             backend=default_backend()
         )
-
-        # Desencriptar a mensagem usando a chave privada
-        decrypted_message = private_key.decrypt(
+        print('Decriptando mensagem...\n')
+        for i in range(3):
+            print(i)
+            time.sleep(1)
+        # Decripta a mensagem usando a chave privada
+        decrypted_message = private_key.decrypt( # semelhante à criptografia
             encrypted_message,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -42,14 +56,14 @@ def main():
             )
         )
 
-        # Decodificar a mensagem desencriptada e printar
-        print("Mensagem desencriptada recebida:", decrypted_message.decode())
+        # Decodifica a mensagem desencriptada e printar
+        print("Mensagem decriptada recebida:", decrypted_message.decode())
 
     except Exception as e:
         print(f"Erro: {e}")
 
     finally:
-        # Fechar o socket do cliente, independentemente de erros
+        # Fecha o socket do cliente, independentemente de erros
         client_socket.close()
 
 if __name__ == "__main__":
